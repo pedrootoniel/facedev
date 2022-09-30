aa98df6b005c7a138f170e8750d76f520e63b56c
pedrootoniel <pedrootonielsantos@outlook.com>
2022-09-30 11:03:07 -0300

    ATUALIZADO 2022

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
 
 axios.defaults.headers['auth_api'] = '5993d7a73d9f9a694e411ba0788cfe2d'
-axios.defaults.baseURL = 'http://api.pedrootoniel.tech/'
-axios.defaults.baseURL = 'https://api.pedrootoniel.tech/'
+axios.defaults.baseURL = 'http://localhost:3000/'
